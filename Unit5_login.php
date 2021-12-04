e542b192a294485d0b152d9950da8450a3471c5a
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-12-04 16:02:45 -0700

    part2

--- Unit5_login.php
+++ Unit5_login.php
@@ -3,9 +3,9 @@
 session_start();
 // // Set session variables
 $_SESSION["role"] = 0;
-// $_SESSION["password"] = "";
+$_SESSION["password"] = "";
 
-// if (empty($_POST['email'] || empty($_POST['password'])){
+if (empty($_POST['email'] || empty($_POST['password'])){
 //         header("Location: Unit5_index.php?err=Invalid User");
-// }
+}
 ?>
