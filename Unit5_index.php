9e0bd97101f49752f8c1fbedb098a5a3dc85b1f8
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-12-04 16:46:42 -0700

    part2

--- Unit5_index.php
+++ Unit5_index.php
@@ -12,7 +12,7 @@
 
 </head>
 <body>
-<p>Welcome! Please login or select Continue as Guest to begin. <?php echo $_GET['err']?></p> 
+<p>Welcome! Please login or select Continue as Guest to begin. <?php if(1==1){echo $_GET['err']}?></p> 
 <form action="Unit5_login.php" method="post">
 
 <fieldset class="login">
